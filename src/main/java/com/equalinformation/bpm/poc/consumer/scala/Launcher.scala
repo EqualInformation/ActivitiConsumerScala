87f04f07d8ec97949a986b3803d4c5f64aff289a
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-11-28 14:13:00 -0800

    Initial structure for vaadin scala using scaladin. Maven compile and build works. Need to fix run time issue.

---
+++ src/main/java/com/equalinformation/bpm/poc/consumer/scala/Launcher.scala
@@ -0,0 +1,32 @@
+package com.equalinformation.bpm.poc.consumer.scala
+
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.webapp.WebAppContext
+
+
+/**
+  * Created by bpupadhyaya on 11/28/15.
+  */
+object Launcher {
+  def main(args: Array[String]): Unit = {
+    try {
+      val root = new WebAppContext();
+      root.setContextPath("/");
+      root.setDescriptor("src/main/webapp/WEB-INF/web.xml");
+      root.setResourceBase("target/activiti-consumer-scala-1.0-SNAPSHOT/WEB-INF/classes");
+      root.setParentLoaderPriority(true);
+
+      println("Starting Jetty Server...")
+      val s = new Server(9000)
+      s.setHandler(root);
+      s.start();
+
+      println("... done!")
+      println("Scaladin application deployed at: http://localhost:9000")
+
+      s.join();
+    } catch {
+      case e: Exception => println("Failed to start the server at port 9000.")
+    }
+  }
+}
